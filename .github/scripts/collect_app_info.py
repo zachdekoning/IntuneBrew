7bebbb95bd52108bda25a2be695aaed09d9639d5
@ugurkocde <43906965+ugurkocde@users.noreply.github.com>
2024-11-05 10:13:29 +0100

    add new app: signal

--- .github/scripts/collect_app_info.py
+++ .github/scripts/collect_app_info.py
@@ -35,7 +35,8 @@ homebrew_cask_urls = [
     "https://formulae.brew.sh/api/cask/mongodb-compass.json",
     "https://formulae.brew.sh/api/cask/suspicious-package.json",
     "https://formulae.brew.sh/api/cask/teamviewer-quicksupport.json",
-    "https://formulae.brew.sh/api/cask/notion.json"
+    "https://formulae.brew.sh/api/cask/notion.json",
+    "https://formulae.brew.sh/api/cask/signal.json"
     ]
 
 # Custom scraper scripts to run
