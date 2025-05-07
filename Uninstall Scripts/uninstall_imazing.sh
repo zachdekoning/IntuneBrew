a61207a3ee3c315b99537f06b19419341a0c3e4d
GitHub Action <action@github.com>
2025-05-07 17:38:03 +0000

    Generated      404 uninstall scripts

--- Uninstall Scripts/uninstall_imazing.sh
+++ Uninstall Scripts/uninstall_imazing.sh
@@ -17,13 +17,13 @@ fi
 echo "Stopping iMazing if running..."
 pkill -f "iMazing" 2>/dev/null || true
 
-# Kill application with bundle ID com.DigiDNA.iMazing3.1.2.21324Mac if running
-echo "Stopping application with bundle ID com.DigiDNA.iMazing3.1.2.21324Mac if running..."
-killall -9 "com.DigiDNA.iMazing3.1.2.21324Mac" 2>/dev/null || true
+# Kill application with bundle ID com.DigiDNA.iMazing3.2.0.21381Mac if running
+echo "Stopping application with bundle ID com.DigiDNA.iMazing3.2.0.21381Mac if running..."
+killall -9 "com.DigiDNA.iMazing3.2.0.21381Mac" 2>/dev/null || true
 
-# Kill application with bundle ID com.DigiDNA.iMazing3.1.2.21324Mac.Mini if running
-echo "Stopping application with bundle ID com.DigiDNA.iMazing3.1.2.21324Mac.Mini if running..."
-killall -9 "com.DigiDNA.iMazing3.1.2.21324Mac.Mini" 2>/dev/null || true
+# Kill application with bundle ID com.DigiDNA.iMazing3.2.0.21381Mac.Mini if running
+echo "Stopping application with bundle ID com.DigiDNA.iMazing3.2.0.21381Mac.Mini if running..."
+killall -9 "com.DigiDNA.iMazing3.2.0.21381Mac.Mini" 2>/dev/null || true
 
 # Remove /Applications/iMazing.app
 echo "Removing /Applications/iMazing.app..."
