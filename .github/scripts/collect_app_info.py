bd9633522786e174e59d4f575205408303fa217e
@ugurkocde <43906965+ugurkocde@users.noreply.github.com>
2025-05-21 23:40:34 +0200

    Revert "Add MySQL Workbench URL to app_urls and remove from homebrew_cask_urls"
    This reverts commit 936800a38757507b41ef4a26e60f75405d1d0fe8.

--- .github/scripts/collect_app_info.py
+++ .github/scripts/collect_app_info.py
@@ -16,7 +16,6 @@ preserve_filename_apps = [
 
 # zip, tar etc
 app_urls = [
-    "https://formulae.brew.sh/api/cask/mysqlworkbench.json",
     "https://formulae.brew.sh/api/cask/signal.json",
     "https://formulae.brew.sh/api/cask/medis.json",
     "https://formulae.brew.sh/api/cask/sourcetree.json",
@@ -181,6 +180,7 @@ app_urls = [
 
 # DMG
 homebrew_cask_urls = [
+    "https://formulae.brew.sh/api/cask/mysqlworkbench.json",
     "https://formulae.brew.sh/api/cask/multiapp.json",
     "https://formulae.brew.sh/api/cask/recut.json",
     "https://formulae.brew.sh/api/cask/firefox@esr.json",
